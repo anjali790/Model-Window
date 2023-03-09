f2b4053260f7641fe0f39bffe688c82f3557248b
anjali790 <anjaliruby790@gmail.com>
2023-03-09 14:15:46 +0530

    model window

--- app.js
+++ app.js
@@ -10,19 +10,25 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener("click", () => {
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
-    })
+    });
 }
 
-btnCloseModal.addEventListener("click", () => {
+const closeModal = function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// document.addEventListener("keydown", () => {
+//     console.log("A key was pressed")
+// });
+
+document.addEventListener("keydown", (e) => {
+    // console.log("A key was pressed",e);
+
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
 });
-
-overlay.addEventListener("click", () => {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-});
-
-document.addEventListener("keydown", ()=>{
-
-})
